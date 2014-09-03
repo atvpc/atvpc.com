929280af1e14d47a63f2d805f53d51fe0bdcb812
Timothy Keith <timothy@keithieopia.com>
2014-09-03 12:17:37 -0400

    Fixed piwik stats url

--- content/themes/ca-webstore/js/piwik.js
+++ content/themes/ca-webstore/js/piwik.js
@@ -2,7 +2,7 @@ var _paq = _paq || [];
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.keithieopia.com/";
+    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://stats.atvpc.com/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', 1]);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
